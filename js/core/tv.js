312d266556b50e592370bb8ce79a7dd4655fbd29
Zach Hall <zach@zachhall.org>
2017-05-03 00:32:01 -0400

    #23 --- Added channel number when page is loaded

--- js/core/tv.js
+++ js/core/tv.js
@@ -3,7 +3,7 @@
  * @Author: zshall 
  * @Date: 2017-05-02 22:34:58 
  * @Last Modified by: zshall
- * @Last Modified time: 2017-05-03 00:14:53
+ * @Last Modified time: 2017-05-03 00:31:21
  */
 const maxWidth = 1200;
 const maxHeight = 1000;
@@ -62,10 +62,15 @@ function showChannel(number, callback) {
     if (window.channel) {
         window.channel.teardown();
     }
+    $('#tvm-top-right').css('opacity', '0');
     $('.current-channel').css('opacity', '0').attr('id', `ch-${number}`).load(`channels/${Helpers.padLeft(number, 3)}/channel.html`, () => {
         window.channel = new classes['Channel' + number]($('.current-channel'), window.guideData);
         channel.show();
-        $('.current-channel').animate({opacity: 1}, firstStart ? warmupTime : warmupTime / 10);
+        $('.current-channel, #tvm-top-right').animate({opacity: 1}, firstStart ? warmupTime : warmupTime / 10);
+        $('#tvm-top-right').text(Helpers.padLeft(number, 2));
+        setTimeout(() => {
+            $('#tvm-top-right').text('');
+        }, 4000);
         firstStart = false;
     });
 }
